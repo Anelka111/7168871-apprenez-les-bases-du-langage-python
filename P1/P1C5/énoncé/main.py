82d8432552743b7b0ef826b1033f1ed1c4edbbc9
Anelka111 <kossianelka1@gmail.com>
2025-03-03 15:16:27 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,10 @@
 # Écrivez votre code ici !
+nom = "Anelka Kossi"
+age = 25
+taille = 1,91
+est_etudiant = true
+print(f{nom} {age} {taille} {est_etudiant})
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
