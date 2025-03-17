6bdb76f679a052ffb7931c0c4883e003e1580371
Anelka111 <kossianelka1@gmail.com>
2025-03-17 15:50:48 +0000

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,6 @@
-# Écrivez votre code ici !
+Fruits: {"pomme":"rouge", "banane":"jaune", "orange":"orange"}
+Fruits[kiwi] = vert
+Fruit['banane]
+Fruit['pomme] = vert
+delFruits["banane]
+printFruits
